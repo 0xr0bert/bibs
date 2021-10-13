4355d6c4c9533d1b9dba79790c2187d0c86638fe
Robert Greener <rob@robgreener.com>
2021-10-13 15:05:06 +0100

    Add behaviour with only a name [#12]
    Signed-off-by: Robert Greener <rob@robgreener.com>

---
+++ include/bibs/behaviour.hpp
@@ -0,0 +1,42 @@
+/* BIBS, the belief-induced behaviour simulation
+ * Copyright (C) 2021 Robert Greener
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BIBS_BEHAVIOUR_H
+#define BIBS_BEHAVIOUR_H
+
+#include <string>
+
+namespace BIBS {
+/**
+ * A behaviour which can be performed.
+ */
+class Behaviour {
+public:
+  /**
+   * The name of the behaviour.
+   */
+  const std::string name;
+  /**
+   * Creates a new behaviour.
+   *
+   * @param name The name of the new behaviour.
+   */
+  Behaviour(const std::string name);
+};
+} // namespace BIBS
+
+#endif // BIBS_BEHAVIOUR_H
